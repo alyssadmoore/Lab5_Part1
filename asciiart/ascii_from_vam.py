08e7ae350c3f23ae517f4f8384260ddeb8d22548
alyssadmoore <alyssadmoore@gmail.com>
2017-02-16 15:31:19 -0600

    Initial commit

---
+++ asciiart/ascii_from_vam.py
@@ -0,0 +1,10 @@
+import asciiart
+from asciiart import asciify
+from asciiart import vam
+
+# Get random image
+print('Fetching a random image from the V&A collection...')
+filename = vam.download_random_vam_image()
+print('done, saved as ' + filename)
+
+asciify.ascii_art(filename)
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='AsciiArt',
+      version='0.1',
+      description='Ascii "art" from images',
+      author='Alyssa',
+      packages=['asciiart'],
+      install_requires=['pillow', 'requests'])
